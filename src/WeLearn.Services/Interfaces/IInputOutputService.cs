120007009f862516dc5ca57692a1064bb76dff2c
kristiyanivanovx <kristianiva88@gmail.com>
2021-06-26 16:03:52 +0300

    Update IInputOutputService.cs

--- src/WeLearn.Services/Interfaces/IInputOutputService.cs
+++ src/WeLearn.Services/Interfaces/IInputOutputService.cs
@@ -8,12 +8,12 @@ namespace WeLearn.Services.Interfaces
 {
     public interface IInputOutputService
     {
-        public Stream ArchiveFiles(IEnumerable<IFormFile> files);
+        Stream ArchiveFiles(IEnumerable<IFormFile> files);
 
-        public Task<Stream> ArchiveFilesAsync(IEnumerable<IFormFile> files);
+        Task<Stream> ArchiveFilesAsync(IEnumerable<IFormFile> files);
 
-        public string GetUniqueFileName(string fileName);
+        string GetUniqueFileName(string fileName);
 
-        public string GenerateItemPath(string root, string subItem, string subSubItem = null);
+        string GenerateItemPath(string root, string subItem, string subSubItem = null);
     }
 }
